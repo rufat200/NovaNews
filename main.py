4799a97b8231655bdb99d33801b7d249d876bb96
rufat200 <habibrahmanow07@mail.ru>
2025-02-23 23:48:32 +0600

    Rewrite: files(in main.py on line 13 added (app.include_router(router=comments_router))

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from src.news import categories_router, news_router
+from src.news import categories_router, news_router, comments_router
 from src.users import users_router
 from src.media import media_router
 
@@ -10,4 +10,5 @@ app = FastAPI()
 app.include_router(router=categories_router)
 app.include_router(router=news_router)
 app.include_router(router=users_router)
+app.include_router(router=comments_router)
 app.include_router(router=media_router)
